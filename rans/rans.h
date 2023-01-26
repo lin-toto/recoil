ee56faf594a3ddadd55a2862639bf0a8dab679fe
Toto Lin <toto@linux.com>
2023-01-26 22:57:37 +0900

    Basic constructs of rans class

--- rans/rans.h
+++ rans/rans.h
@@ -1,9 +1,28 @@
 #ifndef RECOIL_RANS_H
 #define RECOIL_RANS_H
 
-template<typename RansStateType, typename RansOutputType>
-class Rans {
+#include <cstdint>
+#include <type_traits>
 
+template<typename RansStateType, typename RansOutputType,
+        uint8_t ProbBits, RansStateType RenormLowerBound, uint8_t WriteBitCount = 8 * sizeof(RansOutputType)>
+class Rans {
+    static_assert(std::is_unsigned_v<RansStateType>, "RansStateType must be an unsigned type");
+    static_assert(std::is_unsigned_v<RansOutputType>, "RansStateType must be an unsigned type");
+    static_assert(WriteBitCount <= sizeof(RansOutputType) * 8, "WriteBitCount cannot be greater than the size of RansOutputType");
+public:
+    Rans () : state(RenormLowerBound) {}
+    Rans (RansStateType state) : state(state) {}
+protected:
+    RansStateType state;
+
+    static constexpr bool oneShotRenorm = WriteBitCount >= ProbBits;
 };
+
+template<uint8_t ProbBits, uint64_t RenormLowerBound = 1ull << 31>
+using Rans64 = Rans<uint64_t, uint32_t, ProbBits, RenormLowerBound>;
+
+template<uint8_t ProbBits, uint64_t RenormLowerBound = 1u << 16>
+using Rans32 = Rans<uint32_t, uint16_t, ProbBits, RenormLowerBound>;
 
 #endif //RECOIL_RANS_H
---
+++ test.cpp
@@ -0,0 +1,5 @@
+#include "rans/rans.h"
+
+int main() {
+
+}
