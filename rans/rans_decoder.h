8b7d49031de2eb02947cff531db6f3c35a4aa513
Toto Lin <toto@linux.com>
2023-02-03 02:38:46 +0900

    Add basic decoder constructs

---
+++ rans/rans_decoder.h
@@ -0,0 +1,47 @@
+#ifndef RECOIL_RANS_DECODER_H
+#define RECOIL_RANS_DECODER_H
+
+#include "lib/cdf.h"
+#include "rans/rans.h"
+#include <span>
+#include <array>
+
+template<typename RansStateType, typename RansBitstreamType,
+        BitCountType ProbBits, RansStateType RenormLowerBound, BitCountType WriteBits,
+        size_t nInterleaved>
+class RansDecoder {
+    using MyRans = Rans<RansStateType, RansBitstreamType, ProbBits, RenormLowerBound, WriteBits>;
+public:
+    RansDecoder(std::span<RansBitstreamType> bitstream, std::array<MyRans, nInterleaved> rans)
+        : rans(std::move(rans)), bitstream(bitstream),
+        ransIt(this->rans.begin()), bitstreamReverseIt(this->bitstream.rbegin()) {}
+
+    /*
+     * Decode the values with a single shared CDF.
+     * If the count is 0, then all available symbols are decoded.
+     */
+    std::vector<ValueType> decode(Cdf cdf, size_t count = 0) {
+
+    }
+
+    /*
+     * Decode the values with independent CDF for each symbol.
+     */
+    std::vector<ValueType> decode(std::span<Cdf> cdfs) {
+
+    }
+
+
+protected:
+    std::array<MyRans, nInterleaved> rans;
+    std::span<RansBitstreamType> bitstream;
+
+    std::array<MyRans, nInterleaved>::iterator ransIt;
+    std::span<RansBitstreamType>::reverse_iterator bitstreamReverseIt;
+
+    ValueType decodeSymbol(MyRans& decoder, Cdf cdf) {
+
+    }
+};
+
+#endif //RECOIL_RANS_DECODER_H
